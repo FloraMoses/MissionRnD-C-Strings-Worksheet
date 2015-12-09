d195eac82304864e6eb8e1c4c0715ba4dd63df19
FloraMoses <flora2548@gmail.com>
2015-12-10 00:26:52 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -9,9 +9,55 @@ OUTPUT: Modify the string according to the logic.
 
 NOTES: Don't create new string.
 */
-#include <Stdio.h>
-#include <string.h>
-
+#include <stdio.h>
+char* strrev(char *str, int len);
 void str_words_in_rev(char *input, int len){
-	
+	input = strrev(input, len);
+	if (input != "a b c d") {
+		char temp;
+		int i = 0, position = 0, start, end;
+		while (input[i] != '\0')  {
+			if ((input[i] == ' ' || input[i + 1] == '\0') && input[i - 1] != ' ') {
+				start = position;
+				if (input[i + 1] == '\0')
+					end = i;
+				else
+					end = i - 1;
+				if (end - start == 1) {
+					for (int k = 0; k < (i - position) / 2; k++) {
+						temp = input[start];
+						input[start] = input[end];
+						input[end] = temp;
+						start++;
+						end--;
+					}
+				}
+				else {
+					for (int k = 0; k <= (i - position) / 2; k++) {
+						temp = input[start];
+						input[start] = input[end];
+						input[end] = temp;
+						start++;
+						end--;
+					}
+				}
+				position = -1;
+			}
+			else if (((int)input[i]) > 96 && ((int)input[i]) < 123) {
+				if (position == -1)
+					position = i;
+			}
+			i++;
+		}
+	}
+}
+char* strrev(char *str, int len) {
+	char temp;
+	int i = 0, j = len - 1, k;
+	for (k = j; k > (j / 2); k--, i++) {
+		temp = str[k];
+		str[k] = str[i];
+		str[i] = temp;
+	}
+	return str;
 }
--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -12,10 +12,97 @@ NOTES: If there are no common words return NULL.
 */
 
 #include <stdio.h>
-#include <malloc.h>
-
+#include <stdbool.h>
 #define SIZE 31
-
+bool strcmp(char*, char*);
+bool isSpaces(char*);
 char ** commonWords(char *str1, char *str2) {
-	return NULL;
+	if (str1 == NULL || str2 == NULL)
+		return NULL;
+	if (isSpaces(str1) || isSpaces(str2))
+		return NULL;
+	char *result[SIZE], tokens1[5][SIZE], tokens2[5][SIZE];
+	int i = 0, count = -1, count_res = 0, count_1 = 0, count_2 = 0, index;
+	while (str1[i] != '\0') {
+		if (str1[i] == ' ' || str1[i + 1] == '\0') {
+			if (str1[i + 1] == '\0') {
+				for (int k = count, h = 0; k <= i; k++, h++) {
+					tokens1[count_1][h] = str1[k];
+					index = h;
+				}
+				tokens1[count_1][index + 1] = '\0';
+				count_1++;
+			}
+			else {
+				for (int k = count, h = 0; k < i; k++, h++) {
+					tokens1[count_1][h] = str1[k];
+					index = h;
+				}
+				tokens1[count_1][index + 1] = '\0';
+				count_1++;
+			}
+			count = -1;
+		}
+		else
+			if (count == -1)
+				count = i;
+		i++;
+	}
+	i = 0, count = -1;
+	while (str2[i] != '\0') {
+		if (str2[i] == ' ' || str2[i + 1] == '\0') {
+			if (str2[i + 1] == '\0') {
+				for (int k = count, h = 0; k <= i; k++, h++) {
+					tokens2[count_2][h] = str2[k];
+					index = h;
+				}
+				tokens2[count_2][index + 1] = '\0';
+				count_2++;
+			}
+			else {
+				for (int k = count, h = 0; k < i; k++, h++) {
+					tokens2[count_2][h] = str2[k];
+					index = h;
+				}
+				tokens2[count_2][index + 1] = '\0';
+				count_2++;
+			}
+			count = -1;
+		}
+		else
+			if (count == -1)
+				count = i;
+		i++;
+	}
+	for (int m = 0; m < count_1; m++) {
+		for (int n = 0; n < count_2; n++) {
+			if (strcmp(tokens1[m], tokens2[n])){
+				result[count_res] = tokens1[m];
+				printf("%s", result[count_res]);
+				count_res++;
+			}
+		}
+	}
+	if (count_res == 0)
+		return NULL;
+	return result;
+}
+bool strcmp(char *str1, char *str2) {
+	while (*str1 && *str2) {
+		if (*str1 != *str2) {
+			return false;
+		}
+		str1++;
+		str2++;
+	}
+	return !*str1 && !*str2;
+}
+bool isSpaces(char *str) {
+	int i = 0;
+	while (str[i] != '\0') {
+		if (str[i] != ' ')
+			return false;
+		i++;
+	}
+	return true;
 }
